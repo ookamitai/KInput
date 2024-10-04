c3449972cfc4479a533ee4ac059d9f13b07312d2
ookamitai <ookamitai2007@gmail.com>
2024-10-04 12:06:40 +0800

    initial commit

---
+++ KInput/KInput/KInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  KInputApp.swift
+//  KInput
+//
+//  Created by ookamitai on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct KInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
